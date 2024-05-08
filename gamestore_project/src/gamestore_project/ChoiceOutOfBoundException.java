770b4b269b825b6bbc397235030d98c4dd8b045c
EMAN <EMAN@Eman-Laptop>
2024-05-08 21:48:35 +0300

    class user defined exception

---
+++ gamestore_project/src/gamestore_project/ChoiceOutOfBoundException.java
@@ -0,0 +1,15 @@
+package gamestore_project;
+
+public class ChoiceOutOfBoundException extends Exception{
+
+	  public ChoiceOutOfBoundException(){
+	     super();
+	  }
+	  
+	  public ChoiceOutOfBoundException(String message ){
+	     super(message);
+	  }
+	  
+
+
+}
